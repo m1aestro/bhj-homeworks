f32241aae9e39e3e0b80c9f8e0760311b42d5543
Alexandr Avakyan <avakjan@mail.ru>
2021-02-21 22:36:52 +0300

    Second commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,27 @@
 const menuMain = document.querySelector(".menu_main");
 const menuItem = Array.from(menuMain.getElementsByClassName("menu__item"));
+const menu__link = document.querySelectorAll(".menu__link");
 
-function clickMenuLink() {
-    const menuSub = this.querySelector(".menu_sub");   
+menu__link.forEach(function(item) {
+    let menu_sub = item.parentElement.querySelector(".menu_sub");
+    if (menu_sub) {
+        item.onclick = function() {
+            return false;
+        }
+    }
+});
+
+menuItem.forEach(function(item) {
+    item.addEventListener("click", function() {
+        let menu_sub = this.querySelector(".menu_sub");
+        if (menu_sub) {
+            menu_sub.classList.toggle("menu_active");
+        }
     
-    if (!menuSub) {
-        return;
-    }
-
-    const activeMenu = Array.from(document.getElementsByClassName("menu_active"));
-    activeMenu.forEach(element => {
-        if (element !== menuSub) { 
-            element.className = element.className.split(" ").filter((element) => element !== "menu_active").join(" ")}
+        const activeMenu = Array.from(document.getElementsByClassName("menu_active"));
+        activeMenu.forEach(element => {
+            if (element !== menu_sub) { 
+                element.className = element.className.split(" ").filter((element) => element !== "menu_active").join(" ")}
+        });
     });
-
-
-    if (menuSub && !menuSub.className.includes("menu_active")) {
-        menuSub.className += " menu_active";
-        return false;
-    }
-}
-
-menuItem.forEach(element => element.onclick = clickMenuLink);
+});
